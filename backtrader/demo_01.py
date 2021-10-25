943d6e0e33b889773fd52147bd6a3df806888435
JW11235 <wangjiehui11235@163.com>
2021-10-25 20:32:46 +0800

    code update!

--- backtrader/demo_01.py
+++ backtrader/demo_01.py
@@ -4,6 +4,7 @@ backtrader 案例
 @ author: Jeffrey.Wang
 @ date: 2021.10.4
 """
+
 import os
 import datetime
 import pandas as pd
@@ -13,38 +14,70 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['SimHei']
 
 
-class MyStrategy(bt.Strategy):
-    params = (
-        ('ma_short', 5),
-        ('ma_long', 20),
-    )
-
-    def log(self, txt, dt=None):
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+class Platform(bt.Indicator):
+    lines = ('uplimit', 'downlimit')
 
     def __init__(self):
-
-        self.dataclose = self.datas[0].close
-        self.zf = self.datas[0].high - self.datas[0].low
-
-        self.sma = bt.indicators.SimpleMovingAverage(
-            self.datas[0].close, period=self.params.ma_short)
-        self.lma = bt.indicators.SimpleMovingAverage(
-            self.datas[0].close, period=self.params.ma_long)
-
+        self.addminperiod(6)
 
     def next(self):
-        self.log('Close, %.2f' % self.dataclose[0])
+        self.uplimit[0] = max(self.data.high.get(ago=-1, size=5))
+        self.downlimit[0] = min(self.data.low.get(ago=-1, size=5))
 
+
+class MyStrategy(bt.Strategy):
+    params = (
+        ('maperiod_3', 3),
+        ('maperiod_5', 5)
+    )
+
+    def log(self):
+        pass
+
+    def __init__(self):
+        print(f'init___{self.datas[0].datetime.date(0)}')
+        self.limits = Platform(self.data)
+        self.buysignals = bt.indicators.CrossOver(self.datas[0].close, self.limits.uplimit)
+        self.sellsignals = bt.indicators.CrossDown(self.data.close, self.limits.downlimit)
+        # self.order = None
+        self.buysignals.plotinfo.plot = False
+        self.sellsignals.plotinfo.plot = False
+        self.limits.plotinfo.plotmaster = self.data  # 类似通达信的 是否在主图显示
+        # self.卖出信号.plotinfo.plot = False
+
+    def start(self):
+        print(f"start!___{self.datas[0].datetime.date(0)}")
+
+    def prenext(self):
+        print(f"prenext___{self.datas[0].datetime.date(0)}")
+
+    def nextstart(self):
+        print(f'nextstart___{self.datas[0].datetime.date(0)}')
+
+    # def notify_order(self):
+    #     pass
+    #
+    # def notify_trade(self):
+    #     pass
+
+    def next(self):
+        # if self.order:
+        #     return
         if not self.position:
-            if self.lma[0] > self.sma[0]: #  and self.zf[0] > self.dataclose[0] * 0.02
-                self.log('Buy Create, %.2f' % self.dataclose[0])
-                self.buy(size=10000)
+            if self.buysignals[0] == 1:
+                self.order = self.buy(size=1000)
+                print(f"{self.datas[0].datetime.date(0)},买入！价格为{self.data.close[0]}")
         else:
-            if self.lma[0] < self.sma[0]:
-                self.log('Sell Create: %.2f' % self.dataclose[0])
-                self.sell(size=10000)
+            if self.sellsignals[0] == 1:
+                self.order = self.sell(size=1000)
+                print(f"{self.datas[0].datetime.date(0)},卖出！价格为{self.data.close[0]}")
+        pass
+
+    def stop(self):
+        print(f"stop___{self.datas[0].datetime.date(0)}")
+        if self.position:
+            self.order = self.sell(size=1000)
+            print(f"{self.datas[0].datetime.date(0)},卖出！价格为{self.data.close[0]}")
 
 
 if __name__ == '__main__':
@@ -79,7 +112,7 @@ if __name__ == '__main__':
     print('期末资金: {}'.format(end_cash))
     print('夏普率： {}'.format(spratio))
     print('最大回撤： {}'.format(dd['drawdown']))
-    cerebro.plot(style="candle")
+    cerebro.plot()
 
     # for alyzer in strat.analyzers:
     #     alyzer.print()
--- backtrader/strategies.py
+++ backtrader/strategies.py
@@ -68,3 +68,37 @@ class MyStrategy(bt.Strategy):
 
 
 ###############################################
+class MyStrategy(bt.Strategy):
+    params = (
+        ('ma_short', 5),
+        ('ma_long', 20),
+    )
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+
+        self.dataclose = self.datas[0].close
+        self.zf = self.datas[0].high - self.datas[0].low
+
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0].close, period=self.params.ma_short)
+        self.lma = bt.indicators.SimpleMovingAverage(
+            self.datas[0].close, period=self.params.ma_long)
+
+
+    def next(self):
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if not self.position:
+            if self.lma[0] > self.sma[0]: #  and self.zf[0] > self.dataclose[0] * 0.02
+                self.log('Buy Create, %.2f' % self.dataclose[0])
+                self.buy(size=10000)
+        else:
+            if self.lma[0] < self.sma[0]:
+                self.log('Sell Create: %.2f' % self.dataclose[0])
+                self.sell(size=10000)
+
+###############################################
